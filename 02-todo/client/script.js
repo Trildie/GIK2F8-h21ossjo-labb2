a917fa9f0237ed772827835b7b2202ce22d1fb79
Oskar Sjödin <oskar.s@hotmail.com>
2022-12-19 15:39:54 +0100

    laggt till checkbox

--- 02-todo/client/script.js
+++ 02-todo/client/script.js
@@ -1,48 +1,50 @@
-todoForm.title.addEventListener('keyup', (e) => validateField(e.target));
-todoForm.title.addEventListener('blur', (e) => validateField(e.target));
-todoForm.description.addEventListener('input', (e) => validateField(e.target));
-todoForm.description.addEventListener('blur', (e) => validateField(e.target));
-todoForm.dueDate.addEventListener('input', (e) => validateField(e.target));
-todoForm.dueDate.addEventListener('blur', (e) => validateField(e.target));
+todoForm.title.addEventListener("keyup", (e) => validateField(e.target));
+todoForm.title.addEventListener("blur", (e) => validateField(e.target));
+todoForm.description.addEventListener("input", (e) => validateField(e.target));
+todoForm.description.addEventListener("blur", (e) => validateField(e.target));
+todoForm.dueDate.addEventListener("input", (e) => validateField(e.target));
+todoForm.dueDate.addEventListener("blur", (e) => validateField(e.target));
 
-todoForm.addEventListener('submit', onSubmit);
+todoForm.addEventListener("submit", onSubmit);
 
-const todoListElement = document.getElementById('todoList');
+const todoListElement = document.getElementById("todoList");
 
 let titleValid = true;
 let descriptionValid = true;
 let dueDateValid = true;
 
-const api = new Api('http://localhost:5000/tasks');
+const api = new Api("http://localhost:5000/tasks");
 
 function validateField(field) {
   const { name, value } = field;
 
-  let = validationMessage = '';
+  let = validationMessage = "";
 
   switch (name) {
-    case 'title': {
+    case "title": {
       if (value.length < 2) {
         titleValid = false;
         validationMessage = "Fältet 'Titel' måste innehålla minst 2 tecken.";
       } else if (value.length > 100) {
         titleValid = false;
-        validationMessage = "Fältet 'Titel' får inte innehålla mer än 100 tecken.";
+        validationMessage =
+          "Fältet 'Titel' får inte innehålla mer än 100 tecken.";
       } else {
         titleValid = true;
       }
       break;
     }
-    case 'description': {
+    case "description": {
       if (value.length > 500) {
         descriptionValid = false;
-        validationMessage = "Fältet 'Beskrvining' får inte innehålla mer än 500 tecken.";
+        validationMessage =
+          "Fältet 'Beskrvining' får inte innehålla mer än 500 tecken.";
       } else {
         descriptionValid = true;
       }
       break;
     }
-    case 'dueDate': {
+    case "dueDate": {
       if (value.length === 0) {
         dueDateValid = false;
         validationMessage = "Fältet 'Slutförd senast' är obligatorisk.";
@@ -54,13 +56,13 @@ function validateField(field) {
   }
 
   field.previousElementSibling.innerText = validationMessage;
-  field.previousElementSibling.classList.remove('hidden');
+  field.previousElementSibling.classList.remove("hidden");
 }
 
 function onSubmit(e) {
   e.preventDefault();
   if (titleValid && descriptionValid && dueDateValid) {
-    console.log('Submit');
+    console.log("Submit");
     saveTask();
   }
 }
@@ -70,7 +72,7 @@ function saveTask() {
     title: todoForm.title.value,
     description: todoForm.description.value,
     dueDate: todoForm.dueDate.value,
-    completed: false
+    completed: false,
   };
 
   api.create(task).then((task) => {
@@ -81,12 +83,12 @@ function saveTask() {
 }
 
 function renderList() {
-  console.log('rendering');
+  console.log("rendering");
   api.getAll().then((tasks) => {
-    todoListElement.innerHTML = '';
+    todoListElement.innerHTML = "";
     if (tasks && tasks.length > 0) {
       tasks.forEach((task) => {
-        todoListElement.insertAdjacentHTML('beforeend', renderTask(task));
+        todoListElement.insertAdjacentHTML("beforeend", renderTask(task));
       });
     }
   });
@@ -94,10 +96,12 @@ function renderList() {
 
 function renderTask({ id, title, description, dueDate }) {
   let html = `
-    <li class="select-none mt-2 py-2 border-b border-amber-300">
-      <div class="flex items-center">
-        <h3 class="mb-3 flex-1 text-xl font-bold text-pink-800 uppercase">${title}</h3>
-        <div>
+    <li id="skitKurs" class="select-none mt-2 py-2 border-b border-amber-300">
+      <div id="igKURS" class="flex items-center">
+        <h3 id="h3task" class="mb-3 flex-1 text-xl font-bold text-pink-800 uppercase">${title}</h3>
+        <input type="checkbox" onclick="checktBox(${id})" id="completed" name="completed" value="false"> 
+        <label for="completed"> Completed </label>        
+        <div>          
           <span>${dueDate}</span>
           <button onclick="deleteTask(${id})" class="inline-block bg-amber-500 text-xs text-amber-900 border border-white px-3 py-1 rounded-md ml-2">Ta bort</button>
         </div>
@@ -118,4 +122,8 @@ function deleteTask(id) {
   });
 }
 
+
+
+
 renderList();
+
--- 02-todo/server/app.js
+++ 02-todo/server/app.js
@@ -116,8 +116,42 @@ app.delete('/tasks/:id', async (req, res) => {
 /***********************Labb 2 ***********************/
 /* Här skulle det vara lämpligt att skriva en funktion som likt post eller delete tar kan hantera PUT- eller PATCH-anrop (du får välja vilket, läs på om vad som verkar mest vettigt för det du ska göra) för att kunna markera uppgifter som färdiga. Den nya statusen - completed true eller falase - kan skickas i förfrågans body (req.body) tillsammans med exempelvis id så att man kan söka fram en given uppgift ur listan, uppdatera uppgiftens status och till sist spara ner listan med den uppdaterade uppgiften */
 
+/*
+Del 1
+Skapa checkrutan i det grafiska för varje task som läggs in.
+När checkrutan klickas i ska taskets data uppdateras - byta "completed" till true.
+När klickas ut byt till false.
+När checkrutan är markerad, byt färg eller något på task.
+
+PUT - skickar en modifierad version och byter ut orginal versionen.
+PUSH - skickar ett set av instruktioner för att modifiera resusen.
+
+
+
+
+Del 2
+Sortera på datum, den med kortast tid kvar till datum ska ligga först.
+
+
+*/
+app.patch('/tasks/:id', async (req, res) => {
+  console.log(req);
+}
+)
+
+
+function checktBox(id){
+  if(completed == true){
+    document.getElementById("igKURS").style.backgroundColor = "lightblue";
+    //renderList();
+  }
+}
+
+
 /* Observera att all kod rörande backend för labb 2 ska skrivas i denna fil och inte i app.node.js. App.node.js är bara till för exempel från lektion 5 och innehåller inte någon kod som används vidare under lektionerna. */
 /***********************Labb 2 ***********************/
 
 /* Med app.listen säger man åte servern att starta. Första argumentet är port - dvs. det portnummer man vill att servern ska köra på. Det sattes till 5000 på rad 9. Det andra argumentet är en anonym arrow-funktion som körs när servern har lyckats starta. Här skrivs bara ett meddelande ut som berättar att servern kör, så att man får feedback på att allt körts igång som det skulle. */
 app.listen(PORT, () => console.log('Server running on http://localhost:5000'));
+
+
